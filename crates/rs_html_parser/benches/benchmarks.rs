a5f947adbedafa5ecd95c9055bfb21088dae56a2
Sampo Kivistö <sampo.kivisto@live.fi>
2023-11-25 21:44:12 +0200

    Removed wiki page test and replaced it with test_data, also improved benchmark

--- crates/rs_html_parser/benches/benchmarks.rs
+++ crates/rs_html_parser/benches/benchmarks.rs
@@ -1,14 +1,62 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rs_html_parser::{Parser, ParserOptions};
 use rs_html_parser_tokenizer::TokenizerOptions;
-use std::fs;
-use std::path::Path;
+use std::{fs, io};
+use std::path::PathBuf;
+use std::time::Duration;
 
-fn benchmark_wiki_page(c: &mut Criterion) {
-    let file_path = Path::new(
-        "/home/sampo/git/rs_html_parser/crates/rs_html_parser/benches/bench_data/wiki.html",
-    );
-    let contents = fs::read_to_string(file_path).expect("Should have wiki page test data");
+fn get_files_in_folder(path: &str) -> io::Result<Vec<PathBuf>> {
+    let entries = fs::read_dir(path)?;
+    let all: Vec<PathBuf> = entries
+        .filter_map(|entry| Some(entry.ok()?.path()))
+        .collect();
+    Ok(all)
+}
+
+pub fn read_all_test_file_data(path: &str) -> Vec<String> {
+    let mut test_files: Vec<String> = Vec::new();
+    match get_files_in_folder(path) {
+        Ok(files) => {
+            for file in files {
+                if file.is_dir() {
+                    println!("{} is a directory", file.display());
+                    continue;
+                }
+                if file.is_symlink() {
+                    println!("{} is a symlink", file.display());
+                    continue;
+                }
+
+                let Ok(m) = file.metadata() else {
+                    println!("Could not get metadata for {}", file.display());
+                    continue;
+                };
+
+                if m.len() == 0 {
+                    println!("{} is an empty file", file.display());
+                    continue;
+                }
+
+                let file_name = file.to_str().unwrap().to_owned();
+
+                if file_name.ends_with(".html") {
+                    test_files.push(file_name)
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e),
+    };
+
+    let mut test_data: Vec<String> = Vec::new();
+
+    for entry in test_files {
+        test_data.push(fs::read_to_string(entry).unwrap())
+    }
+    test_data
+}
+
+fn benchmark_test_data(c: &mut Criterion) {
+    let test_data = read_all_test_file_data("./../../test_data/");
 
     c.bench_function("benchmarks", |b| {
         b.iter(|| {
@@ -20,12 +68,18 @@ fn benchmark_wiki_page(c: &mut Criterion) {
                 },
             };
 
-            let tokenizer = Parser::new(&contents, &options);
+            for test_data in &test_data {
+                let tokenizer = Parser::new(test_data, &options);
 
-            for _token in tokenizer {}
+                for _token in tokenizer {}
+            }
         });
     });
 }
 
-criterion_group!(benches, benchmark_wiki_page);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(15));
+    targets = benchmark_test_data
+);
 criterion_main!(benches);
